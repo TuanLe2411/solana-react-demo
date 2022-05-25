19ddb9c98e6191f1f34674c01065aeda3501677b
LeTuan <49343179+DDS-TuanLD@users.noreply.github.com>
2022-05-25 09:37:24 +0700

    save

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,19 +4,19 @@ import { getTokenAccount } from '../utils/accounts';
 export const getTokenBalance = async (connection: Connection, userPublicKey: PublicKey, tokenPublicKey: PublicKey): Promise<Number> => {
   const receiverTokenAccountAddress = await getTokenAccount(userPublicKey, tokenPublicKey);
   const receiverTokenAccountInfo = await connection.getParsedAccountInfo(receiverTokenAccountAddress);
-  return Number(receiverTokenAccountInfo.value?.data.parsed.info.tokenAmount.uiAmount);
+  return Number(receiverTokenAccountInfo.value?.data?.parsed?.info?.tokenAmount?.uiAmount);
 }
 
 export const getOwnerOfNft  = async (
   connection: Connection,
-  tokenPublicKey: string
+  tokenPublicKey: PublicKey
 ): Promise<string> => {
   const largestAccounts = await connection.getTokenLargestAccounts(
-    new PublicKey(tokenPublicKey)
+    tokenPublicKey
   );
   const largestAccountInfo = await connection.getParsedAccountInfo(
     largestAccounts.value[0].address
   );
   const ownerInfo = Object.create(largestAccountInfo.value);
-  return ownerInfo.data.parsed.info.owner;
+  return ownerInfo.data?.parsed?.info?.owner;
 };
